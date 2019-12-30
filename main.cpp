598af26203f5a1f26cfb23918d110c3b926115c5
GarrisonTravis <58957083+GarrisonTravis@users.noreply.github.com>
2019-12-30 09:51:30 -0600

    Update main.cpp
    Added tests for empty and size

--- main.cpp
+++ main.cpp
@@ -8,31 +8,47 @@ int main() {
 
 	Stack<int> stack1;
 
+	if (stack1.empty())
+		cout << "Stack is empty." << endl;
+	else
+		cout << "Stack is not empty." << endl;
+	cout << endl;
+
+	cout << "Size of stack: " << stack1.size() << endl << endl;
+
+	cout << "Pushing: 1, 2, 3, 4, 5" << endl;
 	stack1.push(1);
 	stack1.push(2);
 	stack1.push(3);
 	stack1.push(4);
 	stack1.push(5);
 	stack1.print();
+	cout << "Size of stack: " << stack1.size() << endl << endl;
 
 	cout << "Popped: " << stack1.pop() << endl;
 	stack1.print();
+	cout << "Size of stack: " << stack1.size() << endl << endl;
 
 	cout << "Popped: " << stack1.pop() << endl;
 	stack1.print();
+	cout << "Size of stack: " << stack1.size() << endl << endl;
 
 	cout << "Popped: " << stack1.pop() << endl;
 	stack1.print();
+	cout << "Size of stack: " << stack1.size() << endl << endl;
 
 	cout << "Popped: " << stack1.pop() << endl;
 	stack1.print();
+	cout << "Size of stack: " << stack1.size() << endl << endl;
 
 	cout << "Popped: " << stack1.pop() << endl;
 	stack1.print();
+	cout << "Size of stack: " << stack1.size() << endl << endl;
 
 	try {
 		cout << "Popped: " << stack1.pop() << endl;
 		stack1.print();
+		cout << "Size of stack: " << stack1.size() << endl << endl;
 	}
 	catch(exception &e) {
 		cout << "Exception caught! " << e.what() << endl;
